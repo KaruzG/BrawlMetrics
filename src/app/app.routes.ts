ce767f4c61db7101ca13219c9ac1208d10a53b5f
Miguel Gresa <josemiguelcpcs@gmail.com>
2024-11-12 21:57:24 +0100

    Base commit (TailWindCSS, routing)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component'
+import { ContactComponent } from './pages/contact/contact.component'
+import { LegendsComponent } from './pages/legends/legends.component'
+
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'contact', component: ContactComponent},
+    {path: 'legends', component: LegendsComponent},
+];
